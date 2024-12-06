0210646ea79eeb929cd32ed4f1cdc90d73ceca68
duduCMT <dev.eduardo.lc@gmail.com>
2024-12-06 18:26:21 -0300

    add use client in loader overlay provider

--- src/modules/Clean/contexts/LoaderOverlayProvider/index.tsx
+++ src/modules/Clean/contexts/LoaderOverlayProvider/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { createContext, useContext, useState } from 'react';
 import styles from './styles.module.scss';
 import { Spinner, SpinnerProps } from '../../components';
