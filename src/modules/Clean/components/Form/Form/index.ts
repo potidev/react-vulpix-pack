8560b05160cd9bae5b26d2e6f307ef7890344fb5
duduCMT <dev.eduardo.lc@gmail.com>
2024-12-07 14:03:40 -0300

    add use client in form components

--- src/modules/Clean/components/Form/Form/index.ts
+++ src/modules/Clean/components/Form/Form/index.ts
@@ -2,10 +2,7 @@ export * from "./hooks";
 
 export * from "./FormControl";
 export * from "./FormDescription";
-export * from "./FormFieldContext";
-export * from "./FormFieldContext";
 export * from "./FormItem";
-export * from "./FormItemContext";
 export * from "./FormLabel";
 export * from "./FormMessage";
 export * from "./FormProvider";
