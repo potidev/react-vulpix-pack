925ac47c5dd35fbff526ec42b6c4a0927f9772e4
duduCMT <dev.eduardo.lc@gmail.com>
2024-12-10 17:44:15 -0300

    fix main types

--- src/modules/ShadcnUI/components/Sidebar/SidebarMainContainer/types.ts
+++ src/modules/ShadcnUI/components/Sidebar/SidebarMainContainer/types.ts
@@ -3,4 +3,6 @@ import { ReactNode } from "react";
 export type SidebarMainContainerProps = {
   className?: string;
   children?: ReactNode;
+  fixedHeader?: ReactNode;
+  defaultContentPadding?: boolean;
 };
