84bedf750ad6bfd8ea5fde92c4151e9444feb7d5
duduCMT <dev.eduardo.lc@gmail.com>
2024-12-14 19:49:58 -0300

    add table container

---
+++ src/modules/ShadcnUI/components/Table/TableContainer/component.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/modules/ShadcnUI/lib";
+import React from "react";
+
+const TableContainer = React.forwardRef<
+HTMLDivElement,
+ React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "w-full overflow-auto",
+      className
+    )}
+    {...props}
+  />
+))
+TableContainer.displayName = "TableContainer"
+
+export { TableContainer };
