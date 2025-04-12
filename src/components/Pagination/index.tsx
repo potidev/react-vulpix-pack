a8d2bbee6ae36a274d8c9f0aa8f8d05e4c08bebf
duduCMT <dev.eduardo.lc@gmail.com>
2025-04-11 21:44:07 -0300

    pagination label

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -67,8 +67,9 @@ function PaginationLink({
 
 function PaginationPrevious({
   className,
+  label = "Anterior",
   ...props
-}: React.ComponentProps<typeof PaginationLink>) {
+}: React.ComponentProps<typeof PaginationLink> & { label?: string }) {
   return (
     <PaginationLink
       aria-label="Go to previous page"
@@ -77,15 +78,16 @@ function PaginationPrevious({
       {...props}
     >
       <ChevronLeftIcon />
-      <span className="hidden sm:block">Previous</span>
+      <span className="hidden sm:block">{label}</span>
     </PaginationLink>
   )
 }
 
 function PaginationNext({
   className,
+  label = "Próximo",
   ...props
-}: React.ComponentProps<typeof PaginationLink>) {
+}: React.ComponentProps<typeof PaginationLink> & { label?: string }) {
   return (
     <PaginationLink
       aria-label="Go to next page"
@@ -93,7 +95,7 @@ function PaginationNext({
       className={cn("gap-1 px-2.5 sm:pr-2.5", className)}
       {...props}
     >
-      <span className="hidden sm:block">Next</span>
+      <span className="hidden sm:block">{label}</span>
       <ChevronRightIcon />
     </PaginationLink>
   )
@@ -101,8 +103,9 @@ function PaginationNext({
 
 function PaginationEllipsis({
   className,
+  label = "Mais páginas",
   ...props
-}: React.ComponentProps<"span">) {
+}: React.ComponentProps<"span"> & { label?: string }) {
   return (
     <span
       aria-hidden
@@ -111,7 +114,7 @@ function PaginationEllipsis({
       {...props}
     >
       <MoreHorizontalIcon className="size-4" />
-      <span className="sr-only">More pages</span>
+      <span className="sr-only">{label}</span>
     </span>
   )
 }
