19e498c130608b2db9f4c51ae95d04388bde2b24
duduCMT <dev.eduardo.lc@gmail.com>
2025-04-14 11:52:12 -0300

    add new variants in badge

--- src/components/Badge/component.tsx
+++ src/components/Badge/component.tsx
@@ -10,10 +10,16 @@ const badgeVariants = cva(
       variant: {
         default:
           "border-transparent bg-default text-default-foreground shadow-sm hover:bg-default/80",
+        primary:
+          "border-transparent bg-primary text-primary-foreground shadow-sm hover:bg-primary/80",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/80",
+        warning:
+          "border-transparent bg-warning text-warning-foreground shadow-sm hover:bg-warning/80",
+        success:
+          "border-transparent bg-success text-success-foreground shadow-sm hover:bg-success/80",
         outline: "text-foreground",
       },
     },
