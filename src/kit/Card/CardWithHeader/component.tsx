f981803218baf32f94afd1d073c514d3bfb4128e
duduCMT <dev.eduardo.lc@gmail.com>
2025-05-01 12:37:44 -0300

    fix CardWithHeader

--- src/kit/Card/CardWithHeader/component.tsx
+++ src/kit/Card/CardWithHeader/component.tsx
@@ -7,8 +7,8 @@ import { CardWithHeaderProps } from "./types";
 
 export const CardWithHeader = ({ className, header, headerClassName, withoutDefaultPadding, children, ...rest }: CardWithHeaderProps) => {
   return (
-    <Card className={cn("flex flex-col overflow-hidden", className)} withoutDefaultPadding={withoutDefaultPadding} {...rest}>
-      <header className={cn("flex flex-row gap-2 justify-between items-center bg-muted/50 py-2 px-4 border-border border-b-[1px]", headerClassName)}>
+    <Card className={cn("flex flex-col overflow-hidden", className)} withoutDefaultPadding {...rest}>
+      <header className={cn("flex flex-row gap-2 justify-between items-center bg-muted/50 border-border border-b-[1px]", withoutDefaultPadding === true ? "" : "py-2 px-4", headerClassName)}>
         {header}
       </header>
       <article className={cn("grid grid-cols-1 lg:grid-cols-2 gap-4", withoutDefaultPadding === true ? "" : "p-4")}>
