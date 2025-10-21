5bfb6e6db3d3bf6eb9dc850c7719033738dcf237
duduCMT <dev.eduardo.lc@gmail.com>
2025-10-20 22:35:06 -0300

    add color picker

--- src/kit/index.ts
+++ src/kit/index.ts
@@ -1,6 +1,7 @@
 export * from "./Alert";
 export * from "./Button";
 export * from "./Card";
+export * from "./ColorPicker";
 export * from "./Information";
 export * from "./Inputs";
 export * from "./Link";
