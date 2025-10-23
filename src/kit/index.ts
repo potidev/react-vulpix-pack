0ab1fccbf964645d380f75aeab8e7debe2ff2390
duduCMT <dev.eduardo.lc@gmail.com>
2025-10-22 22:07:56 -0300

    remove color picker and add dialog prop to hide close button

--- src/kit/index.ts
+++ src/kit/index.ts
@@ -1,7 +1,6 @@
 export * from "./Alert";
 export * from "./Button";
 export * from "./Card";
-export * from "./ColorPicker";
 export * from "./Information";
 export * from "./Inputs";
 export * from "./Link";
